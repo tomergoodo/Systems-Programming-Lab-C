7b6ab52330a00869c726e7f727ea5c171d71da16
Tomer Goodovitch <tomergoodo@gmail.com>
2020-03-26 08:27:19 -0700

    Revert "Removed Cmake from git"
    This reverts commit c80fe6527e11b5b76aa263ecfc0d4f4e4900a147.

---
+++ Assembler/cmake-build-debug/CMakeFiles/assembler.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assembler.dir/dictionaries.c.o"
+  "CMakeFiles/assembler.dir/first_pass.c.o"
+  "CMakeFiles/assembler.dir/label_table.c.o"
+  "CMakeFiles/assembler.dir/main.c.o"
+  "CMakeFiles/assembler.dir/second_pass.c.o"
+  "CMakeFiles/assembler.dir/utility.c.o"
+  "assembler"
+  "assembler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assembler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
